cfe451f56f9174e43b21c69780e4e693d2a36f22
Tom Graft <tgraft@ebay.com>
2018-07-13 11:50:41 -0400

    Removes flicker

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# hyperatompunk
+# hyperatompunk - No Flickering Version
 
-> Atompunk HyperTerm theme adding RGB shifting, tab text-borders, and CRT effect
+> Atompunk HyperTerm theme adding RGB shifting, tab text-borders, and CRT effect.  This branch does not have the flicker effect (which is visually interesting but somewhat distracting to me after a while)
 
 ![](screenshot.png)
 
 ## Install
 
-Add `hyperatompunk` to the plugins list in your `~/.hyperterm.js` config file.
+Add `hyperatompunk` to the plugins list in your `~/.hyperterm.js` config file, but point to this git repo + branch directly.
 
 This was originally a fork of [hyperpunk](https://github.com/staltz/hyperpunk) by [Andre Staltz](https://staltz.com).
 
--- index.js
+++ index.js
@@ -26,71 +26,6 @@ exports.decorateHyper = (HyperTerm, { React, notify }) => {
         backgroundColor: 'black',
         customCSS: `
           ${this.props.customCSS || ''}
-          @keyframes flicker {
-            0% {
-              opacity: 0.27861;
-            }
-            5% {
-              opacity: 0.34769;
-            }
-            10% {
-              opacity: 0.23604;
-            }
-            15% {
-              opacity: 0.90626;
-            }
-            20% {
-              opacity: 0.18128;
-            }
-            25% {
-              opacity: 0.83891;
-            }
-            30% {
-              opacity: 0.65583;
-            }
-            35% {
-              opacity: 0.67807;
-            }
-            40% {
-              opacity: 0.26559;
-            }
-            45% {
-              opacity: 0.84693;
-            }
-            50% {
-              opacity: 0.96019;
-            }
-            55% {
-              opacity: 0.08594;
-            }
-            60% {
-              opacity: 0.20313;
-            }
-            65% {
-              opacity: 0.71988;
-            }
-            70% {
-              opacity: 0.53455;
-            }
-            75% {
-              opacity: 0.37288;
-            }
-            80% {
-              opacity: 0.71428;
-            }
-            85% {
-              opacity: 0.70419;
-            }
-            90% {
-              opacity: 0.7003;
-            }
-            95% {
-              opacity: 0.36108;
-            }
-            100% {
-              opacity: 0.24387;
-            }
-          }
           ${TEXT_SHADOW}
           .hyper_main::after {
             content: " ";
@@ -104,7 +39,6 @@ exports.decorateHyper = (HyperTerm, { React, notify }) => {
             opacity: 0;
             z-index: 2;
             pointer-events: none;
-          	animation: flicker 0.15s infinite;
           }
           .hyper_main::before {
             content: " ";
